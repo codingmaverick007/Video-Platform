68b4243003e8158194ad35434eca7b13360f15e9
codingmaverick007 <87696487+codingmaverick007@users.noreply.github.com>
2024-06-11 19:50:09 +0000

    added a default from email

--- video_platform/video_platform/settings.py
+++ video_platform/video_platform/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path, os
 import dj_database_url
 import socket
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -212,6 +213,8 @@ EMAIL_HOST_USER = 'kevboat963@gmail.com'
 EMAIL_HOST_PASSWORD = 'owmt tikn fqzj wvyl'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='Kelvin\'s Company <your-email@gmail.com>')
 
 GRAPH_MODELS = {
   'all_applications': True,
